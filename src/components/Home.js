8969f4856616e05a2bc4c4fe63ca0c1637dac243
Macklemoon <m-moon@csu.fullerton.edu>
2021-04-10 10:53:52 -0700

    now uploads properly.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import {storage, db} from "./config";
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import {storage, db} from "./config";
 
 function Home() {
     const [users, setUsers] = useState([])
+    const [url, setURL] = useState("");
     const fetchUsers = async() => {
         const response = db.collection('users');
         const data = await response.get();
         data.docs.forEach(item=> {
-            setUsers([...users, item.data()])
+            setUsers([...users, item.data()]);
         })
     }
 
@@ -23,23 +24,23 @@ function Home() {
       
       async function getImg(user) {
         var str = "images/" + user.profilePicture;
-        const img = await storage.ref().child(str).getDownloadURL();
-        console.log(img);
-        return img;
+        storage.ref().child(str).getDownloadURL().then((url) => {
+            setURL(url);
+          });
       }
 
       return (
         <div>
             {
                 users && users.map(user=> {
+                    getImg(user);
                     //<li> here gets rid of the returns, but would fix a harmless error on console.
-                    console.log(getImg(user));
                     return(
                         <div className = "users-container">
                             <h1>{user.username}</h1>
                             <Link to={`/singleview/${user.username}`}>
                                 <Picture>
-                                    <img src= {getImg(user)} alt="profile" />
+                                    <img src= {url} alt="profile" />
                                 </Picture>
                             </Link>
                         </div>

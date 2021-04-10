8e3823e1596de66cf2d6209376d13a41c1599984
Macklemoon <m-moon@csu.fullerton.edu>
2021-04-10 10:13:32 -0700

    updating...

--- src/components/config.js
+++ src/components/config.js
@@ -3,7 +3,13 @@ import "firebase/storage";
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyB9rC0qRG6-qYO1JcAaES98rLq-TlhVoVE",
+    authDomain: "onlycats2-4d448.firebaseapp.com",
+    databaseURL: "https://onlycats2-4d448-default-rtdb.firebaseio.com",
+    projectId: "onlycats2-4d448",
+    storageBucket: "onlycats2-4d448.appspot.com",
+    messagingSenderId: "684644430823",
+    appId: "1:684644430823:web:e86817bc0eafe298f0c342"
 };
 
 firebase.initializeApp(firebaseConfig)

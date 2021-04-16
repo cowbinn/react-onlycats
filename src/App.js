13c69737658c51a35551c03398ce549015161f62
kevinlbnguyen113 <36432653+kevinlbnguyen113@users.noreply.github.com>
2021-04-15 21:47:08 -0700

    login
    login console log user logging in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
+import firebase, { auth } from './components/config.js'
 import Header from './components/Header.js'
 import Favorite from './components/Favorite.js'
 import Profile from './components/Profile.js'
@@ -16,6 +17,15 @@ import { BrowserRouter as Router,Switch, Route } from "react-router-dom"
 import Uploadpage from './components/Uploadpage'
 
 function App() {
+  firebase.auth().onAuthStateChanged(function (user){
+    if(user){
+      console.log("im logged in fucker");
+    }
+    else {
+      console.log("not log in")
+    }
+  });
+  
   return (
     <Router>
       <div className="App">
